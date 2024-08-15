94af14fe3652c419857afcd0682ec7a22abfff3c
jinbeizame007 <jinbeizame007@gmail.com>
2024-08-15 20:47:35 +0900

    [add] AbstractValue and Value

---
+++ src/common/value.rs
@@ -0,0 +1,43 @@
+use std::any::Any;
+
+pub trait AbstractValue {
+    fn as_any(&self) -> &dyn Any;
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+    fn clone_box(&self) -> Box<dyn AbstractValue>;
+}
+
+#[derive(Clone)]
+pub struct Value<T: Clone> {
+    value: T,
+}
+
+impl<T: 'static + Clone> Value<T> {
+    fn new(value: T) -> Self {
+        Value { value }
+    }
+}
+
+impl<T: 'static + Clone> AbstractValue for Value<T> {
+    fn as_any(&self) -> &dyn Any {
+        &self.value
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        &mut self.value
+    }
+
+    fn clone_box(&self) -> Box<dyn AbstractValue> {
+        Box::new(self.clone())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_constructor() {
+        let value = Value::<i64>::new(22);
+        assert_eq!(22, *value.as_any().downcast_ref::<i64>().unwrap());
+    }
+}
