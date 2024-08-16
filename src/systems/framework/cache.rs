12a4f0b8d2994f77e45811d991f7d491372bd142
jinbeizame007 <jinbeizame007@gmail.com>
2024-08-16 19:54:04 +0900

    [add] CacheEntryValue and Cache

---
+++ src/systems/framework/cache.rs
@@ -0,0 +1,82 @@
+use crate::common::value::{AbstractValue, Value};
+use crate::systems::framework::framework_common::CacheIndex;
+
+#[derive(Clone, PartialEq)]
+enum Flag {
+    ReadyToUse,
+    ValueIsOutOfDate,
+}
+
+#[derive(Clone)]
+pub struct CacheEntryValue {
+    cache_index: CacheIndex,
+    value: Box<dyn AbstractValue>,
+    flag: Flag,
+}
+
+impl CacheEntryValue {
+    pub fn cache_index(&self) -> &CacheIndex {
+        &self.cache_index
+    }
+
+    pub fn get_abstract_value(&mut self) -> &mut dyn AbstractValue {
+        self.value.as_mut()
+    }
+
+    pub fn mark_up_to_date(&mut self) {
+        self.flag = Flag::ReadyToUse;
+    }
+
+    pub fn mark_out_of_date(&mut self) {
+        self.flag = Flag::ValueIsOutOfDate;
+    }
+
+    pub fn needs_recomputation(&self) -> bool {
+        self.flag == Flag::ValueIsOutOfDate
+    }
+}
+
+impl Default for CacheEntryValue {
+    fn default() -> Self {
+        CacheEntryValue {
+            cache_index: CacheIndex::new(0),
+            value: Box::new(Value::<usize>::new(0)),
+            flag: Flag::ValueIsOutOfDate,
+        }
+    }
+}
+
+pub struct Cache<'a> {
+    store: Vec<CacheEntryValue>,
+    _marker: std::marker::PhantomData<&'a CacheEntryValue>,
+}
+
+impl<'a> Cache<'a> {
+    pub fn cache_size(&self) -> usize {
+        self.store.len()
+    }
+
+    pub fn create_new_cache_entry_value(&mut self, cache_index: CacheIndex) {
+        if cache_index >= self.cache_size() {
+            self.store
+                .resize_with(cache_index.value() + 1, Default::default)
+        }
+
+        let cache_entry_value = CacheEntryValue {
+            cache_index: cache_index.clone(),
+            ..Default::default()
+        };
+        self.store[cache_index] = cache_entry_value;
+    }
+
+    pub fn get_cache_entry_value(&'a self, cache_index: CacheIndex) -> Option<&'a CacheEntryValue> {
+        self.store.get(cache_index.value())
+    }
+
+    pub fn get_mutable_cache_entry_value(
+        &mut self,
+        cache_index: CacheIndex,
+    ) -> Option<&mut CacheEntryValue> {
+        self.store.get_mut(cache_index.value())
+    }
+}
