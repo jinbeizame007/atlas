44bd26af09b88ef6351bb1c6980680437cd3d5e7
jinbeizame007 <jinbeizame007@gmail.com>
2024-08-19 21:52:42 +0900

    [add] ValueProducer

---
+++ src/systems/framework/value_producer.rs
@@ -0,0 +1,20 @@
+use crate::common::value::AbstractValue;
+use crate::systems::framework::context_base::ContextBase;
+
+pub type AllocateCallback = dyn Fn() -> Box<dyn AbstractValue>;
+pub type CalcCallback = dyn Fn(&dyn ContextBase, &mut dyn AbstractValue);
+
+pub struct ValueProducer {
+    allocate_: Box<AllocateCallback>,
+    calc_: CalcCallback,
+}
+
+impl ValueProducer {
+    pub fn allocate(&self) -> Box<dyn AbstractValue> {
+        (self.allocate_)()
+    }
+
+    pub fn calc(&self, context: &dyn ContextBase, value: &mut dyn AbstractValue) {
+        (self.calc_)(context, value)
+    }
+}
