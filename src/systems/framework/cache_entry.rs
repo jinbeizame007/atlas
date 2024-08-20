2c6586797d3c13886849589a24112d916856cb3f
jinbeizame007 <jinbeizame007@gmail.com>
2024-08-20 21:02:45 +0900

    [add] CacheEntry

---
+++ src/systems/framework/cache_entry.rs
@@ -0,0 +1,66 @@
+use crate::common::value::AbstractValue;
+use crate::systems::framework::cache::CacheEntryValue;
+use crate::systems::framework::context_base::ContextBase;
+use crate::systems::framework::framework_common::CacheIndex;
+use crate::systems::framework::value_producer::ValueProducer;
+
+pub struct CacheEntry {
+    cache_index: CacheIndex,
+    value_producer: ValueProducer,
+}
+
+impl CacheEntry {
+    pub fn allocate(&self) -> Box<dyn AbstractValue> {
+        self.value_producer.allocate()
+    }
+
+    pub fn calc(&self, context: &dyn ContextBase, value: &mut dyn AbstractValue) {
+        self.value_producer.calc(context, value)
+    }
+
+    pub fn eval<'a, ValueType: 'static>(&self, context: &'a mut dyn ContextBase) -> &'a ValueType {
+        let abstract_value = self.eval_abstract(context);
+        abstract_value.as_any().downcast_ref::<ValueType>().unwrap()
+    }
+
+    fn eval_abstract<'a>(&self, context: &'a mut dyn ContextBase) -> &'a dyn AbstractValue {
+        if self.get_cache_entry_value(context).needs_recomputation() {
+            self.update_value(context)
+        }
+        self.get_cache_entry_value(context).get_abstract_value()
+    }
+
+    fn update_value(&self, context: &mut dyn ContextBase) {
+        let mut value = {
+            let mutable_cache_value = self.get_mutable_cache_entry_value(context);
+            mutable_cache_value.get_mutable_abstract_value().clone_box()
+        };
+        self.calc(context, value.as_mut());
+
+        let mutable_abstract_value = self
+            .get_mutable_cache_entry_value(context)
+            .get_mutable_abstract_value();
+        mutable_abstract_value.set_from(value.as_ref());
+    }
+
+    pub fn get_cache_entry_value<'a>(&self, context: &'a dyn ContextBase) -> &'a CacheEntryValue {
+        context
+            .get_cache()
+            .get_cache_entry_value(&self.cache_index)
+            .unwrap()
+    }
+
+    pub fn get_mutable_cache_entry_value<'a>(
+        &self,
+        context: &'a mut dyn ContextBase,
+    ) -> &'a mut CacheEntryValue {
+        context
+            .get_mutable_cache()
+            .get_mutable_cache_entry_value(&self.cache_index)
+            .unwrap()
+    }
+
+    pub fn cache_index(&self) -> &CacheIndex {
+        &self.cache_index
+    }
+}
