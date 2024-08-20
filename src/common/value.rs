5b4ea04308df197a6ed10901011ae56a09629864
jinbeizame007 <jinbeizame007@gmail.com>
2024-08-20 20:52:33 +0900

    [add] impl set_value() into AbstractValue

--- src/common/value.rs
+++ src/common/value.rs
@@ -3,6 +3,7 @@ use std::any::Any;
 pub trait AbstractValue {
     fn as_any(&self) -> &dyn Any;
     fn as_any_mut(&mut self) -> &mut dyn Any;
+    fn set_from(&mut self, abstract_value: &dyn AbstractValue);
     fn clone_box(&self) -> Box<dyn AbstractValue>;
 }
 
@@ -15,15 +16,32 @@ impl<T: 'static + Clone> Value<T> {
     pub fn new(value: T) -> Self {
         Value { value }
     }
+
+    pub fn get_value(&self) -> &T {
+        &self.value
+    }
+
+    pub fn set_value(&mut self, value: &T) {
+        self.value = value.clone();
+    }
 }
 
 impl<T: 'static + Clone> AbstractValue for Value<T> {
     fn as_any(&self) -> &dyn Any {
-        &self.value
+        self
     }
 
     fn as_any_mut(&mut self) -> &mut dyn Any {
-        &mut self.value
+        self
+    }
+
+    fn set_from(&mut self, abstract_value: &dyn AbstractValue) {
+        let value = abstract_value
+            .as_any()
+            .downcast_ref::<Value<T>>()
+            .unwrap()
+            .get_value();
+        self.set_value(value);
     }
 
     fn clone_box(&self) -> Box<dyn AbstractValue> {
@@ -45,5 +63,22 @@ mod tests {
     fn test_constructor() {
         let value = Value::<i64>::new(22);
         assert_eq!(22, *value.as_any().downcast_ref::<i64>().unwrap());
+    }
+
+    #[test]
+    fn test_set_from() {
+        let value1 = Value::<i64>::new(11);
+        let mut abstract_value1 = Box::new(value1) as Box<dyn AbstractValue>;
+        let value2 = Value::<i64>::new(22);
+        let abstract_value2 = Box::new(value2) as Box<dyn AbstractValue>;
+        abstract_value1.set_from(abstract_value2.as_ref());
+        assert_eq!(
+            22,
+            *abstract_value1
+                .as_any()
+                .downcast_ref::<Value<i64>>()
+                .unwrap()
+                .get_value()
+        )
     }
 }
