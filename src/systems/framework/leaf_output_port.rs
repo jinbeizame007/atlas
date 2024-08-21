cc5ff62515f5280903fd6a791fa960575c62dc1c
jinbeizame007 <jinbeizame007@gmail.com>
2024-08-21 18:16:10 +0900

    [add] LeafOutputPort

---
+++ src/systems/framework/leaf_output_port.rs
@@ -0,0 +1,83 @@
+use std::cmp::PartialEq;
+use std::fmt::Debug;
+use std::ops::Add;
+
+use num_traits::identities::Zero;
+
+use crate::common::value::AbstractValue;
+use crate::systems::framework::cache_entry::CacheEntry;
+use crate::systems::framework::context::Context;
+use crate::systems::framework::framework_common::{OutputPortIndex, PortDataType, SystemId};
+use crate::systems::framework::output_port::OutputPort;
+use crate::systems::framework::output_port_base::OutputPortBase;
+use crate::systems::framework::port_base::PortBase;
+use crate::systems::framework::system::System;
+
+pub struct LeafOutputPort<'a, T: Add + PartialEq + Clone + Debug + Zero> {
+    system: &'a dyn System<T>,
+    _system_id: SystemId,
+    index: OutputPortIndex,
+    data_type: PortDataType,
+    size: usize,
+    cache_entry: &'a CacheEntry,
+}
+
+impl<'a, T: Add + PartialEq + Clone + Debug + Zero> PortBase for LeafOutputPort<'a, T> {
+    fn get_data_type(&self) -> &PortDataType {
+        &self.data_type
+    }
+
+    fn size(&self) -> usize {
+        self.size
+    }
+}
+
+impl<'a, T: Add + PartialEq + Clone + Debug + Zero> OutputPortBase for LeafOutputPort<'a, T> {
+    fn get_index(&self) -> &OutputPortIndex {
+        &self.index
+    }
+}
+
+impl<'a, T: Add + PartialEq + Clone + Debug + Zero> OutputPort<'a, T> for LeafOutputPort<'a, T> {
+    fn allocate(&mut self) -> Box<dyn AbstractValue> {
+        self.cache_entry.allocate()
+    }
+
+    fn eval<ValueType: Clone + 'static>(&self, context: &mut dyn Context<T>) -> ValueType {
+        self.cache_entry
+            .eval::<ValueType>(context.as_mutable_base())
+            .clone()
+    }
+
+    fn calc(&self, context: &dyn Context<T>, value: &mut dyn AbstractValue) {
+        self.cache_entry.calc(context.as_base(), value)
+    }
+
+    fn get_system(&self) -> &'a dyn System<T> {
+        self.system
+    }
+}
+
+impl<'a, T: Add + PartialEq + Clone + Debug + Zero> LeafOutputPort<'a, T> {
+    pub fn new(
+        system: &'a dyn System<T>,
+        _system_id: SystemId,
+        index: OutputPortIndex,
+        data_type: PortDataType,
+        size: usize,
+        cache_entry: &'a CacheEntry,
+    ) -> Self {
+        LeafOutputPort::<T> {
+            system,
+            _system_id,
+            index,
+            data_type,
+            size,
+            cache_entry,
+        }
+    }
+
+    pub fn cache_entry(&self) -> &'a CacheEntry {
+        self.cache_entry
+    }
+}
