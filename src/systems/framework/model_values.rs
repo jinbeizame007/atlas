76ca04e46df1acd6fa2db36550f947b77d672a02
jinbeizame007 <jinbeizame007@gmail.com>
2024-08-23 19:53:03 +0900

    [add] ModelValues

---
+++ src/systems/framework/model_values.rs
@@ -0,0 +1,57 @@
+use std::cmp::PartialEq;
+use std::fmt::Debug;
+use std::ops::Add;
+
+use num_traits::identities::Zero;
+
+use crate::common::value::{AbstractValue, Value};
+use crate::systems::framework::basic_vector::BasicVector;
+
+// https://en.wikipedia.org/wiki/Prototype_pattern
+pub struct ModelValues {
+    values: Vec<Box<dyn AbstractValue>>,
+}
+
+impl ModelValues {
+    pub fn size(&self) -> usize {
+        self.values.len()
+    }
+
+    pub fn add_model(&mut self, index: usize, model_value: Box<dyn AbstractValue>) {
+        if index >= self.size() {
+            self.values.resize_with(index, Default::default)
+        }
+        self.values[index] = model_value;
+    }
+
+    pub fn add_vector_model<T: Add + PartialEq + Clone + Debug + Default + Zero + 'static>(
+        &mut self,
+        index: usize,
+        model_vector: BasicVector<T>,
+    ) {
+        self.add_model(index, Box::new(Value::<BasicVector<T>>::new(model_vector)));
+    }
+
+    pub fn clone_model(&self, index: usize) -> Box<dyn AbstractValue> {
+        self.values[index].clone()
+    }
+
+    pub fn clone_all_models(&self) -> Vec<Box<dyn AbstractValue>> {
+        self.values.iter().map(|x| x.clone_box()).collect()
+    }
+
+    pub fn clone_vector_model<T: Add + PartialEq + Clone + Debug + Default + Zero + 'static>(
+        &self,
+        index: usize,
+    ) -> BasicVector<T> {
+        let abstract_value = self.clone_model(index);
+        let basic_vector = abstract_value
+            .as_ref()
+            .as_any()
+            .downcast_ref::<BasicVector<T>>()
+            .unwrap()
+            .clone();
+
+        basic_vector
+    }
+}
