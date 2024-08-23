aa7836fdeda7c68a83c3235d2dd2788f2220967c
jinbeizame007 <jinbeizame007@gmail.com>
2024-08-23 19:55:06 +0900

    [add] impl Default for Identifier

--- src/common/identifier.rs
+++ src/common/identifier.rs
@@ -1,12 +1,12 @@
 use std::marker::PhantomData;
 
-#[derive(Clone)]
-pub struct Identifier<Tag> {
+#[derive(Clone, Default)]
+pub struct Identifier<Tag: Default> {
     value: usize,
     _marker: PhantomData<Tag>,
 }
 
-impl<Tag> Identifier<Tag> {
+impl<Tag: Default> Identifier<Tag> {
     pub fn new(value: usize) -> Self {
         Identifier::<Tag> {
             value,
