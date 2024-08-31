27183007418b115cf808cc802e6b8833ea3a8ecf
jinbeizame007 <jinbeizame007@gmail.com>
2024-08-31 09:01:11 +0900

    [fix] apply derive macro for LeafSystem

--- atlas/src/systems/primitives/adder.rs
+++ atlas/src/systems/primitives/adder.rs
@@ -1,4 +1,4 @@
-use atlas_derives::SystemBase;
+use atlas_derives::LeafSystem;
 
 extern crate nalgebra as na;
 
@@ -25,7 +25,7 @@ use crate::systems::framework::system_base::ContextSizes;
 use crate::systems::framework::system_base::SystemBase;
 use crate::systems::framework::vector_base::VectorBase;
 
-#[derive(SystemBase)]
+#[derive(LeafSystem)]
 pub struct Adder<T: AtlasScalar> {
     #[allow(clippy::box_collection)]
     input_ports: Box<Vec<InputPort<T>>>,
@@ -37,161 +37,6 @@ pub struct Adder<T: AtlasScalar> {
     time_derivatives_cache_index: CacheIndex,
     model_input_values: ModelValues,
     model_continuous_state_vector: BasicVector<T>,
-}
-
-// impl<T: AtlasScalar> SystemBase for Adder<T> {
-//     fn input_ports(&self) -> Vec<&dyn InputPortBase> {
-//         self.input_ports
-//             .iter()
-//             .map(|p| p as &dyn InputPortBase)
-//             .collect()
-//     }
-
-//     fn input_ports_mut(&mut self) -> Vec<&mut dyn InputPortBase> {
-//         self.input_ports
-//             .iter_mut()
-//             .map(|p| p as &mut dyn InputPortBase)
-//             .collect()
-//     }
-
-//     fn output_ports(&self) -> Vec<&dyn OutputPortBase> {
-//         self.output_ports
-//             .iter()
-//             .map(|p| p.as_ref() as &dyn OutputPortBase)
-//             .collect()
-//     }
-
-//     fn output_ports_mut(&mut self) -> Vec<&mut dyn OutputPortBase> {
-//         self.output_ports
-//             .iter_mut()
-//             .map(|p| p.as_mut() as &mut dyn OutputPortBase)
-//             .collect()
-//     }
-
-//     // fn add_input_port(&mut self, input_port: Box<InputPort<T>>) {
-//     //     self.input_ports.push(input_port);
-//     // }
-
-//     fn cache_entries(&self) -> &Vec<CacheEntry> {
-//         &self.cache_entries
-//     }
-
-//     fn cache_entries_mut(&mut self) -> &mut Vec<CacheEntry> {
-//         &mut self.cache_entries
-//     }
-
-//     fn context_sizes(&self) -> &ContextSizes {
-//         &self.context_sizes
-//     }
-
-//     fn context_sizes_mut(&mut self) -> &mut ContextSizes {
-//         &mut self.context_sizes
-//     }
-
-//     fn system_id(&self) -> &SystemId {
-//         &self.system_id
-//     }
-
-//     fn parent_service(&self) -> Option<&dyn SystemParentServiceInterface> {
-//         self.parent_service.as_ref().map(|p| p.as_ref())
-//     }
-// }
-
-impl<T: AtlasScalar> System<T> for Adder<T> {
-    fn input_ports(&self) -> Vec<&InputPort<T>> {
-        self.input_ports.iter().collect()
-    }
-
-    fn input_ports_mut(&mut self) -> Vec<&mut InputPort<T>> {
-        self.input_ports.iter_mut().collect()
-    }
-
-    fn input_port(&self, index: &InputPortIndex) -> &InputPort<T> {
-        &self.input_ports[index]
-    }
-
-    fn input_port_mut(&mut self, index: &InputPortIndex) -> &mut InputPort<T> {
-        &mut self.input_ports[index]
-    }
-
-    fn add_input_port(&mut self, input_port: InputPort<T>) {
-        self.input_ports.push(input_port);
-    }
-
-    fn output_ports(&self) -> Vec<&dyn OutputPort<T>> {
-        self.output_ports
-            .iter()
-            .map(|p| p.as_ref() as &dyn OutputPort<T>)
-            .collect()
-    }
-
-    fn output_ports_mut(&mut self) -> Vec<&mut dyn OutputPort<T>> {
-        self.output_ports
-            .iter_mut()
-            .map(|p| p.as_mut() as &mut dyn OutputPort<T>)
-            .collect()
-    }
-
-    fn output_port(&self, index: &OutputPortIndex) -> &dyn OutputPort<T> {
-        self.output_ports[index].as_ref()
-    }
-
-    fn output_port_mut(&mut self, index: &OutputPortIndex) -> &mut dyn OutputPort<T> {
-        self.output_ports[index].as_mut()
-    }
-
-    fn time_derivatives_cache_index(&self) -> &CacheIndex {
-        &self.time_derivatives_cache_index
-    }
-
-    fn allocate_context(&self) -> Box<dyn Context<T>> {
-        LeafSystem::<T>::allocate_context(self)
-    }
-
-    fn do_allocate_input(&self, input_port: &InputPort<T>) -> Box<dyn AbstractValue> {
-        LeafSystem::<T>::do_allocate_input(self, input_port)
-    }
-
-    fn allocate_time_derivatives(&mut self) -> ContinuousState<T> {
-        LeafSystem::<T>::allocate_time_derivatives(self)
-    }
-
-    fn set_default_state(&self, context: &mut dyn Context<T>) {
-        LeafSystem::<T>::set_default_state(self, context)
-    }
-}
-
-impl<T: AtlasScalar> LeafSystem<T> for Adder<T> {
-    fn model_input_values(&self) -> &ModelValues {
-        &self.model_input_values
-    }
-
-    fn model_input_values_mut(&mut self) -> &mut ModelValues {
-        &mut self.model_input_values
-    }
-
-    fn model_continuous_state_vector(&self) -> &BasicVector<T> {
-        &self.model_continuous_state_vector
-    }
-
-    fn model_continuous_state_vector_mut(&mut self) -> &mut BasicVector<T> {
-        &mut self.model_continuous_state_vector
-    }
-
-    fn leaf_output_port(&self, output_port_index: &OutputPortIndex) -> &LeafOutputPort<T> {
-        &self.output_ports[output_port_index]
-    }
-
-    fn leaf_output_port_mut(
-        &mut self,
-        output_port_index: &OutputPortIndex,
-    ) -> &mut LeafOutputPort<T> {
-        &mut self.output_ports[output_port_index]
-    }
-
-    fn add_output_port(&mut self, output_port: Box<LeafOutputPort<T>>) {
-        self.output_ports.push(output_port);
-    }
 }
 
 impl<T: AtlasScalar> Adder<T> {
