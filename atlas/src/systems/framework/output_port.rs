f652711c1cd5b182059debdf2dbcfe25b16c7701
jinbeizame007 <jinbeizame007@gmail.com>
2024-09-02 20:42:41 +0900

    [add] associated type for OutputPort

--- atlas/src/systems/framework/output_port.rs
+++ atlas/src/systems/framework/output_port.rs
@@ -4,8 +4,10 @@ use crate::systems::framework::context::Context;
 use crate::systems::framework::output_port_base::OutputPortBase;
 
 pub trait OutputPort<T: AtlasScalar>: OutputPortBase {
+    type CN: Context<T>;
+
     // fn eval<ValueType: Clone + 'static>(&self, context: &mut dyn Context<T>) -> ValueType;
-    fn eval_abstract(&self, context: &mut dyn Context<T>) -> Box<dyn AbstractValue>;
+    fn eval_abstract(&self, context: &mut Self::CN) -> Box<dyn AbstractValue>;
     fn allocate(&mut self) -> Box<dyn AbstractValue>;
-    fn calc(&self, context: &mut dyn Context<T>, value: &mut dyn AbstractValue);
+    fn calc(&self, context: &mut Self::CN, value: &mut dyn AbstractValue);
 }
