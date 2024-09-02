10a5854c8357d2e141a85a58ead8fb417f1b1565
jinbeizame007 <jinbeizame007@gmail.com>
2024-09-02 20:41:38 +0900

    [add] associated type for Context

--- atlas/src/systems/framework/context.rs
+++ atlas/src/systems/framework/context.rs
@@ -1,19 +1,20 @@
 use crate::common::atlas_scalar::AtlasScalar;
 use crate::systems::framework::context_base::ContextBase;
-use crate::systems::framework::continuous_state::ContinuousState;
 use crate::systems::framework::state::State;
 use crate::systems::framework::vector_base::VectorBase;
 
 pub trait Context<T: AtlasScalar>: ContextBase {
+    type S: State<T>;
+
     fn time(&self) -> &T;
-    fn state(&self) -> &State<T>;
-    fn state_mut(&mut self) -> &mut State<T>;
-    fn init_continuous_state(&mut self, continuous_state: ContinuousState<T>);
+    fn state(&self) -> &Self::S;
+    fn state_mut(&mut self) -> &mut Self::S;
+    fn init_continuous_state(&mut self, continuous_state: Box<<Self::S as State<T>>::CS>);
     fn num_continuous_states(&self) -> usize;
-    fn continuous_state(&self) -> &ContinuousState<T>;
-    fn continuous_state_mut(&mut self) -> &mut ContinuousState<T>;
-    fn set_continuous_state(&mut self, continuous_state: ContinuousState<T>) {
-        *self.continuous_state_mut() = continuous_state;
+    fn continuous_state(&self) -> &<Self::S as State<T>>::CS;
+    fn continuous_state_mut(&mut self) -> &mut <Self::S as State<T>>::CS;
+    fn set_continuous_state(&mut self, continuous_state: Box<<Self::S as State<T>>::CS>) {
+        *self.continuous_state_mut() = *continuous_state;
     }
     fn continuous_state_vector(&self) -> &dyn VectorBase<T, Output = T>;
     fn continuous_state_vector_mut(&mut self) -> &mut dyn VectorBase<T, Output = T>;
