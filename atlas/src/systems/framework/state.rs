ff2eaefc2a2e9fb31875efe597ad9d258276623f
jinbeizame007 <jinbeizame007@gmail.com>
2024-09-02 20:42:58 +0900

    [add] associated type for State

--- atlas/src/systems/framework/state.rs
+++ atlas/src/systems/framework/state.rs
@@ -1,21 +1,10 @@
 use crate::common::atlas_scalar::AtlasScalar;
 use crate::systems::framework::continuous_state::ContinuousState;
 
-#[derive(Default)]
-pub struct State<T: AtlasScalar> {
-    continuous_state: ContinuousState<T>,
-}
+pub trait State<T: AtlasScalar> {
+    type CS: ContinuousState<T>;
 
-impl<T: AtlasScalar> State<T> {
-    pub fn new(continuous_state: ContinuousState<T>) -> Self {
-        State::<T> { continuous_state }
-    }
+    fn continuous_state(&self) -> &Self::CS;
 
-    pub fn continuous_state(&self) -> &ContinuousState<T> {
-        &self.continuous_state
-    }
-
-    pub fn continuous_state_mut(&mut self) -> &mut ContinuousState<T> {
-        &mut self.continuous_state
-    }
+    fn continuous_state_mut(&mut self) -> &mut Self::CS;
 }
