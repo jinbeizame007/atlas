5fb250abc9f6ed712542cf7305f14cca04c99971
jinbeizame007 <jinbeizame007@gmail.com>
2024-09-02 20:39:20 +0900

    [add] impl ContinuousState for DiagramContinuousState

--- atlas/src/systems/framework/diagram_continuous_state.rs
+++ atlas/src/systems/framework/diagram_continuous_state.rs
@@ -3,38 +3,186 @@ extern crate nalgebra as na;
 use crate::common::atlas_scalar::AtlasScalar;
 use crate::systems::framework::continuous_state::ContinuousState;
 use crate::systems::framework::framework_common::SystemId;
+use crate::systems::framework::leaf_continuous_state::LeafContinuousState;
+use crate::systems::framework::vector_base::VectorBase;
+
+pub enum ContinuousStatePtr<T: AtlasScalar> {
+    LeafContinuousStatePtr(*mut LeafContinuousState<T>),
+    DiagramContinuousStatePtr(*mut DiagramContinuousState<T>),
+}
+
+impl<T: AtlasScalar> ContinuousStatePtr<T> {
+    fn num_q(&self) -> usize {
+        match self {
+            ContinuousStatePtr::LeafContinuousStatePtr(ptr) => unsafe {
+                ptr.as_ref().unwrap().num_q()
+            },
+            ContinuousStatePtr::DiagramContinuousStatePtr(ptr) => unsafe {
+                ptr.as_ref().unwrap().num_q()
+            },
+        }
+    }
+
+    fn num_v(&self) -> usize {
+        match self {
+            ContinuousStatePtr::LeafContinuousStatePtr(ptr) => unsafe {
+                ptr.as_ref().unwrap().num_v()
+            },
+            ContinuousStatePtr::DiagramContinuousStatePtr(ptr) => unsafe {
+                ptr.as_ref().unwrap().num_v()
+            },
+        }
+    }
+
+    fn num_z(&self) -> usize {
+        match self {
+            ContinuousStatePtr::LeafContinuousStatePtr(ptr) => unsafe {
+                ptr.as_ref().unwrap().num_z()
+            },
+            ContinuousStatePtr::DiagramContinuousStatePtr(ptr) => unsafe {
+                ptr.as_ref().unwrap().num_z()
+            },
+        }
+    }
+
+    fn size(&self) -> usize {
+        match self {
+            ContinuousStatePtr::LeafContinuousStatePtr(ptr) => unsafe {
+                ptr.as_ref().unwrap().size()
+            },
+            ContinuousStatePtr::DiagramContinuousStatePtr(ptr) => unsafe {
+                ptr.as_ref().unwrap().size()
+            },
+        }
+    }
+
+    fn system_id(&self) -> &SystemId {
+        match self {
+            ContinuousStatePtr::LeafContinuousStatePtr(ptr) => unsafe {
+                ptr.as_ref().unwrap().system_id()
+            },
+            ContinuousStatePtr::DiagramContinuousStatePtr(ptr) => unsafe {
+                ptr.as_ref().unwrap().system_id()
+            },
+        }
+    }
+
+    fn set_system_id(&mut self, system_id: SystemId) {
+        match self {
+            ContinuousStatePtr::LeafContinuousStatePtr(ptr) => unsafe {
+                ptr.as_mut().unwrap().set_system_id(system_id)
+            },
+            ContinuousStatePtr::DiagramContinuousStatePtr(ptr) => unsafe {
+                ptr.as_mut().unwrap().set_system_id(system_id)
+            },
+        }
+    }
+
+    fn set_from_vector(&mut self, value: &na::DVector<T>) {
+        match self {
+            ContinuousStatePtr::LeafContinuousStatePtr(ptr) => unsafe {
+                ptr.as_mut().unwrap().set_from_vector(value)
+            },
+            ContinuousStatePtr::DiagramContinuousStatePtr(ptr) => unsafe {
+                ptr.as_mut().unwrap().set_from_vector(value)
+            },
+        }
+    }
+
+    fn vector(&self) -> &dyn VectorBase<T, Output = T> {
+        match self {
+            ContinuousStatePtr::LeafContinuousStatePtr(ptr) => unsafe {
+                ptr.as_ref().unwrap().vector()
+            },
+            ContinuousStatePtr::DiagramContinuousStatePtr(ptr) => unsafe {
+                ptr.as_ref().unwrap().vector()
+            },
+        }
+    }
+
+    fn vector_mut(&mut self) -> &mut dyn VectorBase<T, Output = T> {
+        match self {
+            ContinuousStatePtr::LeafContinuousStatePtr(ptr) => unsafe {
+                ptr.as_mut().unwrap().vector_mut()
+            },
+            ContinuousStatePtr::DiagramContinuousStatePtr(ptr) => unsafe {
+                ptr.as_mut().unwrap().vector_mut()
+            },
+        }
+    }
+}
 
 #[derive(Default)]
 pub struct DiagramContinuousState<T: AtlasScalar> {
-    substates: Vec<*mut ContinuousState<T>>,
+    state: Box<dyn VectorBase<T, Output = T>>,
+    substates: Vec<ContinuousStatePtr<T>>,
+    num_q: usize,
+    num_v: usize,
+    num_z: usize,
     system_id: SystemId,
 }
 
 impl<T: AtlasScalar> DiagramContinuousState<T> {
-    pub fn new(substates: Vec<*mut ContinuousState<T>>) -> Self {
-        DiagramContinuousState::<T> {
-            substates,
-            system_id: SystemId::default(),
-        }
+    pub fn new(substates: Vec<ContinuousStatePtr<T>>) -> Self {
+        todo!()
+        // let state = Box::new(BasicVector
+        // DiagramContinuousState::<T> {
+        //     substates,
+        //     state:
+        //     num_q: 0,
+        //     num_v: 0,
+        //     num_z: 0,
+        //     system_id: SystemId::default(),
+        // }
     }
 
-    pub fn num_substates(&self) -> usize {
+    fn num_substates(&self) -> usize {
         self.substates.len()
     }
 
-    pub fn substate(&self, index: usize) -> &ContinuousState<T> {
-        unsafe { &*self.substates[index] }
+    fn substate(&self, index: usize) -> &ContinuousStatePtr<T> {
+        unsafe { &self.substates[index] }
     }
 
-    pub fn substate_mut(&mut self, index: usize) -> &mut ContinuousState<T> {
-        unsafe { &mut *self.substates[index] }
+    fn substate_mut(&mut self, index: usize) -> &mut ContinuousStatePtr<T> {
+        unsafe { &mut self.substates[index] }
+    }
+}
+
+impl<T: AtlasScalar> ContinuousState<T> for DiagramContinuousState<T> {
+    fn num_q(&self) -> usize {
+        self.num_q
     }
 
-    pub fn system_id(&self) -> &SystemId {
+    fn num_v(&self) -> usize {
+        self.num_v
+    }
+
+    fn num_z(&self) -> usize {
+        self.num_z
+    }
+
+    fn size(&self) -> usize {
+        self.num_q + self.num_v + self.num_z
+    }
+
+    fn system_id(&self) -> &SystemId {
         &self.system_id
     }
 
-    pub fn set_system_id(&mut self, system_id: SystemId) {
+    fn set_system_id(&mut self, system_id: SystemId) {
         self.system_id = system_id;
+    }
+
+    fn set_from_vector(&mut self, value: &na::DVector<T>) {
+        todo!()
+    }
+
+    fn vector(&self) -> &dyn VectorBase<T, Output = T> {
+        todo!()
+    }
+
+    fn vector_mut(&mut self) -> &mut dyn VectorBase<T, Output = T> {
+        todo!()
     }
 }
