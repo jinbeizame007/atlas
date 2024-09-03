b618f3c8ed00d6a72dd17a3272fb7e1ebbecb302
jinbeizame007 <jinbeizame007@gmail.com>
2024-09-03 19:49:04 +0900

    [add] LeafState

---
+++ atlas/src/systems/framework/leaf_state.rs
@@ -0,0 +1,26 @@
+use crate::common::atlas_scalar::AtlasScalar;
+use crate::systems::framework::leaf_continuous_state::LeafContinuousState;
+use crate::systems::framework::state::State;
+
+#[derive(Default)]
+pub struct LeafState<T: AtlasScalar> {
+    continuous_state: Box<LeafContinuousState<T>>,
+}
+
+impl<T: AtlasScalar> LeafState<T> {
+    pub fn new(continuous_state: Box<LeafContinuousState<T>>) -> Self {
+        Self { continuous_state }
+    }
+}
+
+impl<T: AtlasScalar> State<T> for LeafState<T> {
+    type CS = LeafContinuousState<T>;
+
+    fn continuous_state(&self) -> &Self::CS {
+        &self.continuous_state.as_ref()
+    }
+
+    fn continuous_state_mut(&mut self) -> &mut Self::CS {
+        self.continuous_state.as_mut()
+    }
+}
