87f5a453cb8a7ecbdfc628e88e383ae116eb18d7
jinbeizame007 <jinbeizame007@gmail.com>
2024-09-08 14:47:35 +0900

    [add] connect() method into DiagramBuilder

--- atlas/src/common/atlas_scalar.rs
+++ atlas/src/common/atlas_scalar.rs
@@ -1,6 +1,5 @@
-use std::fmt::Debug;
-
 use num_traits::NumAssign;
+use std::fmt::Debug;
 
 pub trait AtlasScalar: NumAssign + Clone + Debug + Default + 'static {}
 
--- atlas/src/systems/framework/diagram_builder.rs
+++ atlas/src/systems/framework/diagram_builder.rs
@@ -5,10 +5,10 @@ use crate::systems::framework::diagram::{
     Diagram, InputPortLocator, OutputPortLocator, OwnedSystems, SystemPtr,
 };
 use crate::systems::framework::diagram_context::DiagramContext;
-use crate::systems::framework::framework_common::{
-    InputPortIndex, OutputPortIndex, SubsystemIndex,
-};
+use crate::systems::framework::framework_common::PortDataType;
+use crate::systems::framework::input_port::InputPort;
 use crate::systems::framework::leaf_context::LeafContext;
+use crate::systems::framework::output_port::OutputPort;
 use crate::systems::framework::system::System;
 
 #[derive(Default)]
@@ -24,6 +24,24 @@ pub struct DiagramBuilder<T: AtlasScalar> {
 impl<T: AtlasScalar> DiagramBuilder<T> {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    pub fn systems(&self) -> &HashSet<SystemPtr<T>> {
+        &self.systems
+    }
+
+    pub fn systems_mut(&mut self) -> &mut HashSet<SystemPtr<T>> {
+        &mut self.systems
+    }
+
+    pub fn connection_map(&self) -> &HashMap<InputPortLocator<T>, OutputPortLocator<T>> {
+        &self.connection_map
+    }
+
+    pub fn connection_map_mut(
+        &mut self,
+    ) -> &mut HashMap<InputPortLocator<T>, OutputPortLocator<T>> {
+        &mut self.connection_map
     }
 
     pub fn add_leaf_system<S>(&mut self, mut system: Box<S>) -> SystemPtr<T>
@@ -50,5 +68,41 @@ impl<T: AtlasScalar> DiagramBuilder<T> {
         self.owned_systems.push(system);
 
         system_ptr
+    }
+
+    pub fn connect<O>(&mut self, output_port: &mut O, input_port: &InputPort<T>)
+    where
+        O: OutputPort<T>,
+    {
+        self.assert_if_already_built();
+
+        let input_port_locator = InputPortLocator::<T> {
+            system_ptr: input_port.system_ptr(),
+            input_port_index: input_port.index().clone(),
+        };
+        let output_port_locator = OutputPortLocator::<T> {
+            system_ptr: output_port.system_ptr(),
+            output_port_index: output_port.index().clone(),
+        };
+
+        if *output_port.data_type() == PortDataType::AbstractValued {
+            let model_output = output_port.allocate();
+            let model_input = input_port.system_ptr().allocate_input_abstract(input_port);
+            if model_output.type_id() != model_input.type_id() {
+                panic!(
+                    "Mismatched value types while connecting output port (type {:?}) to input port (type {:?})",
+                    model_output.type_id(),
+                    model_input.type_id()
+                );
+            }
+        }
+        self.connection_map
+            .insert(input_port_locator, output_port_locator);
+    }
+
+    pub fn assert_if_already_built(&self) {
+        if self.already_built {
+            panic!("DiagramBuilder already built");
+        }
     }
 }
