8bd6904f87327a39c1173f1725cf33881af658a5
jinbeizame007 <jinbeizame007@gmail.com>
2025-05-25 17:10:15 +0900

    [add] prelude

--- atlas/src/lib.rs
+++ atlas/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod common;
+pub mod prelude;
 pub mod systems;
---
+++ atlas/src/prelude.rs
@@ -0,0 +1,5 @@
+pub use crate::systems::framework::basic_vector::BasicVector;
+pub use crate::systems::framework::diagram::{Diagram, DiagramExt};
+pub use crate::systems::framework::diagram_builder::DiagramBuilder;
+pub use crate::systems::framework::framework_common::{InputPortIndex, OutputPortIndex};
+pub use crate::systems::framework::system::System;
